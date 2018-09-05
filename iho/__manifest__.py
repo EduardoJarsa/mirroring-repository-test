7d3d0f955b6bbc5e738f565fa4a06999a874940d
Sarai Osorio <sarai.osorio@jarsa.com.mx>
2018-09-05 15:32:42 -0500

    [ADD][iho]-new module

---
+++ iho/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'IHO customs',
+    'summary': 'Customs',
+    'version': '11.0.1.0.2',
+    'category': 'Connector',
+    'author': 'Jarsa Sistemas S.A. de C.V.',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+}
