55eeafbf9021df62081356bc730b50ebbcc014d2
Sarai Osorio <sarai.osorio@jarsa.com.mx>
2018-11-05 14:21:40 -0600

    [FIX][sif_interface]lint

--- sif_interface/models/stock_rule.py
+++ sif_interface/models/stock_rule.py
@@ -1,7 +1,7 @@
 # Copyright 2018, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class StockRule(models.Model):
--- sif_interface/wizard/export_sif_wizard.py
+++ sif_interface/wizard/export_sif_wizard.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import base64
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 MAPPING = {
     'default_code': 'PN',
