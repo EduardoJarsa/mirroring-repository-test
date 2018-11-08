4b86413d8adf004dd5d067d35a02e51f8942ee3d
lmgr0312 <lmiguel.gruiz@gmail.com>
2018-11-08 17:12:50 -0600

    [ADD][iho_security]Module

---
+++ iho_security/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'IHO Security',
+    'summary': 'IHO Security',
+    'version': '12.0.1.0.0',
+    'category': 'Security',
+    'author': 'Jarsa Sistemas, Vauxoo, Odoo Community Association (OCA)',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'AGPL-3',
+    'depends': [
+        'crm',
+        'sale_crm',
+        'iho',
+    ],
+    'data': [
+        'security/crm_security.xml',
+        'security/ir.model.access.csv',
+        'views/ir_ui_menu_view.xml',
+    ],
+}
