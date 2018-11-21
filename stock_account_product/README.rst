714d946e4d802dd7d400dba44ba710fcecb67f51
lmgr0312 <lmiguel.gruiz@gmail.com>
2018-11-21 09:19:13 -0600

    [ADD][stock_account_product]Change account_move_line names to product names instead of kit main product name

---
+++ stock_account_product/README.rst
@@ -0,0 +1,19 @@
+Stock Account Product
+=====================
+
+This module change the account_move_line names
+
+
+Installation
+============
+
+- This module depends on:
+
+  No extra depends
+
+Credits
+=======
+
+**Contributors**
+* Miguel Ruiz <miguel.ruiz@jarsa.com.mx> (Developer)
+* Jarsa Sistemas SA de CV <info@jarsa.com.mx>
---
+++ stock_account_product/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Account Products',
+    'summary': 'Stock Account Products',
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'author': 'Jarsa Sistemas, Vauxoo',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock_account',
+    ],
+}
