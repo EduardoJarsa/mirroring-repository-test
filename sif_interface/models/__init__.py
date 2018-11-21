16c24ae6cba3b4f38978a75ef663840ce6c9c8df
ougc27 <oscar.garza@jarsa.com.mx>
2018-11-21 11:52:39 -0600

    [REF][sif_interface] - Avoid purchase order line merge process.

--- sif_interface/models/__init__.py
+++ sif_interface/models/__init__.py
@@ -4,6 +4,7 @@
 from . import mrp_bom
 from . import product_supplierinfo
 from . import product_template
+from . import purchase_order
 from . import sale_order
 from . import stock_move
 from . import stock_rule
