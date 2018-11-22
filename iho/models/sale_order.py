c493eabef97de886c97cf32523d1d1cf9d8f90a1
lmgr0312 <lmiguel.gruiz@gmail.com>
2018-11-22 10:16:45 -0600

    [ADD][Sale Order]Add analytic tags so to sol

--- iho/models/sale_order.py
+++ iho/models/sale_order.py
@@ -18,12 +18,23 @@ class SaleOrder(models.Model):
         ondelete='restrict',
         help='The route selected on this field will be passed '
         'to all the sale order lines of this quotation.',)
+    analytic_tag_ids = fields.Many2many(
+        'account.analytic.tag',
+        string='Analytic Tags',
+    )
 
     @api.onchange('route_id')
     def _onchange_route_id(self):
         if self.route_id:
             self.order_line.update({
                 'route_id': self.route_id.id,
+            })
+
+    @api.onchange('analytic_tag_ids')
+    def _onchange_analytic_ids(self):
+        if self.analytic_tag_ids:
+            self.order_line.update({
+                'analytic_tag_ids': self.analytic_tag_ids.ids,
             })
 
 
@@ -34,3 +45,8 @@ class SaleOrderLine(models.Model):
     def _onchange_product_id_set_route(self):
         if self.order_id.route_id:
             self.route_id = self.order_id.route_id.id
+
+    @api.onchange('product_id')
+    def _onchange_product_id_set_analytic_tags(self):
+        if self.order_id.route_id:
+            self.analytic_tag_ids = self.order_id.analytic_tag_ids.ids
