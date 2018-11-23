9ec01acaca6acd6f4d3cf2351a05a8f9a42afafd
lmgr0312 <lmiguel.gruiz@gmail.com>
2018-11-23 11:42:09 -0600

    [ADD][Sale Order]Dynamic sequence by sales_team

---
+++ sale_order_sequence/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Sale Order Sequence',
+    'summary': 'Dynamic sale order sequence by sales team',
+    'version': '12.0.1.0.0',
+    'category': 'sale',
+    'author': 'Jarsa Sistemas',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/crm_team_view.xml',
+    ],
+}
---
+++ sale_order_sequence/models/sale_order.py
@@ -0,0 +1,17 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.model
+    def create(self, vals):
+        if vals['team_id']:
+            team_obj = self.env['crm.team']
+            team = team_obj.browse(vals['team_id'])
+            if team.sequence_id:
+                vals['name'] = team.sequence_id.next_by_id()
+        return super().create(vals)
