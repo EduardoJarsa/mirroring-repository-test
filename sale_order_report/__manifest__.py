e7e5ae207d58b87194f88e5308c6c0c84a91ebb9
Isabel Esparza <isabel.esparza@jarsa.com.mx>
2018-12-19 16:57:15 -0600

    [WIP][sale_order_report] add termns by data and logic for 2 diferentes reports

--- sale_order_report/__manifest__.py
+++ sale_order_report/__manifest__.py
@@ -13,8 +13,11 @@
         'sale_management',
     ],
     'data': [
+        'data/default_template.xml',
         'views/sale_order_view.xml',
         'views/sale_order_template.xml',
+        #'views/sale_order_internal_template.xml',
         'reports/report_papaerformat.xml',
+        #'reports/report_action.xml',
     ],
 }
