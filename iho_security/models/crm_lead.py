76d1eb1c7ba58dafa6fba7d715cbd0603de499a0
Isabel Esparza <isabel.esparza@jarsa.com.mx>
2019-02-27 09:52:42 -0600

    [ADD][iho_security]add a new group for edit  the name of oportunity Task#1905

---
+++ iho_security/models/crm_lead.py
@@ -0,0 +1,20 @@
+# Copyright 2019, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+
+class Lead(models.Model):
+    _inherit = "crm.lead"
+
+    @api.multi
+    def write(self, vals):
+        # Name change: Message for the user
+        # if no have the group access to modify.
+        if 'name' in vals:
+            if not self.user_has_groups(
+                    'iho_security.group_sale_salesman_opportunities'):
+                raise ValidationError(_(
+                    'You cannot modify the name, contact to your admin.'))
+        return super(Lead, self).write(vals)
