56fa4ed7544bf335b31de98d7409a4aef0ff225e
Isabel Esparza <isabel.esparza@jarsa.com.mx>
2019-03-22 11:36:27 -0600

    [FIX][sale_review]Add sudo() for problems with rules//task#1948

--- sale_review/models/sale_order.py
+++ sale_review/models/sale_order.py
@@ -13,12 +13,12 @@ class SaleOrder(models.Model):
     def review_sale_order(self):
         self.ensure_one()
         if self.review_so:
-            self.review_so = False
+            self.sudo().review_so = False
             message = _('<strong>%s</strong> canceled review'
                         ' <strong>%s %s</strong>.') % (
                 self.user_id.name, self.name, self.active_version_id.name,)
         else:
-            self.review_so = True
+            self.sudo().review_so = True
             message = _('The <strong>%s</strong>'
                         ' was reviewed by <strong>%s</strong>.') % (
                 self.name, self.user_id.name, )
