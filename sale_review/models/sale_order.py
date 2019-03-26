30971ba4a80e04353945a9fb249544af2d7b69db
Isabel Esparza <isabel.esparza@jarsa.com.mx>
2019-03-26 16:02:05 -0600

    [FIX][sale_review]Fix user name on the chatter to review one SO// task#1948

--- sale_review/models/sale_order.py
+++ sale_review/models/sale_order.py
@@ -12,18 +12,20 @@ class SaleOrder(models.Model):
     @api.multi
     def review_sale_order(self):
         self.ensure_one()
+        current_uid = self._context.get('uid')
+        user = self.env['res.users'].browse(current_uid)
         if self.review_so:
             self.sudo().review_so = False
             message = _('<strong>%s</strong> canceled review'
                         ' <strong>%s %s</strong>.') % (
-                self.user_id.name, self.name, self.active_version_id.name,)
+                user.name, self.name, self.active_version_id.name,)
         else:
             self.sudo().review_so = True
             message = _('The <strong>%s</strong>'
                         ' was reviewed by <strong>%s</strong>.') % (
-                self.name, self.user_id.name, )
+                self.name, user.name, )
             if self.active_version_id:
                 message = _('The <strong>%s %s</strong> '
                             'was reviewed by <strong>%s</strong>.') % (
-                    self.name, self.active_version_id.name, self.user_id.name,)
+                    self.name, self.active_version_id.name, user.name,)
         return self.message_post(body=message)
