33f2e23344c989a4e378862e37fc30dd932a9ec9
jesus01x <xzan12@outlook.com>
2019-12-30 12:32:47 -0600

    [CHANGE] sale_order_template: change css to logo of company has auto adjust

--- ipdb/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import res_company
-from . import res_config_settings
-from . import sale_order
