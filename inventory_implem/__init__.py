4a720a0e3bfded7dad293ae8a1ea241554b0e512
Hector Herrera <hector.herrera.mx@gmail.com>
2020-10-20 19:15:22 -0600

    [ADD] inventory_implem: Avoiding to register lines in SO if product is not available

--- inventory_implem/__init__.py
+++ inventory_implem/__init__.py
@@ -1,2 +1,4 @@
 # Copyright 2018, MtNet Services, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
---
+++ inventory_implem/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2020, MtNet Services, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import sale_order_line
