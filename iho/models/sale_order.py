c8a46c4e0feced3a00f97cd4ac5be7c8887e062c
Hector Herrera <hector.herrera.mx@gmail.com>
2020-12-08 11:56:13 -0700

    [ADD]iho: avoids to have SO lines with qtty as 1

--- iho/models/sale_order.py
+++ iho/models/sale_order.py
@@ -1,7 +1,9 @@
 # Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# Copyright 2020, MtNet Services, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -35,3 +37,11 @@ class SaleOrderLine(models.Model):
         self.invalidate_cache(
             fnames=['analytic_tag_ids'], ids=[self.id])
         self.analytic_tag_ids = self.order_id.analytic_tag_ids.ids
+
+    @api.constrains('product_uom_qty')
+    def _onchange_product_uom_qty(self):
+        if self.product_uom_qty == 1:
+            raise ValidationError(
+                _('Error: Column "Quantity" at [%s] has value of [%s] '
+                  'and must NOT be [1]') %
+                (self._product_int_ref(), self.product_uom_qty))
