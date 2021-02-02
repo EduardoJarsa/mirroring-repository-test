dd2f905544983cc085372bc9722177e78b755d20
Hector Herrera <hector.herrera.mx@gmail.com>
2021-02-02 08:52:10 -0700

    [ADD]sif_interface mtnet licensing added

--- sif_interface/models/sale_order_line.py
+++ sif_interface/models/sale_order_line.py
@@ -1,4 +1,4 @@
-# Copyright 2018, MtNet Services, S.A. de C.V.
+# Copyright 2018, 2021 MtNet Services, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import _, api, fields, models
