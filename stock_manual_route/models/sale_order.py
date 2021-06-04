c1009f8a26e719c91aff3dd89e6b5bad014974f7
jesus01x <xzan12@outlook.com>
2021-06-04 09:22:12 -0500

    [FIX] stock_manual_route: fixed empty analytic and analytic_tag_ids in pickings type out.

--- stock_manual_route/models/sale_order.py
+++ stock_manual_route/models/sale_order.py
@@ -8,17 +8,31 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    def _unlink_pincking_int(self, picking_int):
+        if picking_int:
+            picking_int.write({
+                'state': 'draft',
+            })
+            picking_int.move_lines.write({
+                'state': 'draft',
+            })
+            picking_int.unlink()
+
+    def _put_account_and_tags_in_moves(self, picking_out):
+        if picking_out:
+            picking_out.move_ids_without_package.write({
+                'analytic_tag_ids': self.analytic_tag_ids.ids,
+                'analytic_account_id': self.analytic_account_id.id,
+            })
+
     def action_confirm(self):
         res = super().action_confirm()
-        picking = self.picking_ids.filtered(
+        picking_int = self.picking_ids.filtered(
             lambda l: l.picking_type_id.code == 'internal'
             and l.picking_type_id.sequence_code == 'INT')
-        if picking:
-            picking.write({
-                'state': 'draft',
-            })
-            picking.move_lines.write({
-                'state': 'draft',
-            })
-            picking.unlink()
+        picking_out = self.picking_ids.filtered(
+            lambda l: l.picking_type_id.code == 'outgoing'
+            and l.picking_type_id.sequence_code == 'OUT')
+        self._unlink_pincking_int(picking_int)
+        self._put_account_and_tags_in_moves(picking_out)
         return res
