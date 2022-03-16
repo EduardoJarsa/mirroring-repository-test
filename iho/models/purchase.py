f86dd24d92d77d8a433a322d6f9fcbdb7544f942
Hector Herrera <hector.herrera.mx@gmail.com>
2022-03-15 22:43:01 -0600

    [ADD]iho adding usd fields for po subtotal and total

--- iho/models/purchase.py
+++ iho/models/purchase.py
@@ -1,7 +1,7 @@
 # Copyright 2020, MtNet Services, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import models, fields, api
 
 
 class PurchaseOrder(models.Model):
@@ -11,3 +11,34 @@ class PurchaseOrder(models.Model):
         vals = super().action_view_picking()
         self.message_unsubscribe([self.partner_id.id])
         return vals
+
+    amount_untaxed_usd = fields.Float(
+        string='usd Untaxed',
+        compute='_compute_amount_untaxed_usd',
+        store=True,
+    )
+    amount_total_usd = fields.Float(
+        string='usd Total',
+        compute='_compute_amount_total_usd',
+        store=True,
+    )
+
+    @api.depends('amount_untaxed')
+    def _compute_amount_untaxed_usd(self):
+        for rec in self:
+            rec.amount_untaxed_usd = rec.currency_id._convert(
+                rec.amount_untaxed,
+                self.env.ref('base.USD'),
+                rec.company_id,
+                rec.date_approve or fields.Date.today()
+            )
+
+    @api.depends('amount_total')
+    def _compute_amount_total_usd(self):
+        for rec in self:
+            rec.amount_total_usd = rec.currency_id._convert(
+                rec.amount_total,
+                self.env.ref('base.USD'),
+                rec.company_id,
+                rec.date_approve or fields.Date.today()
+            )
