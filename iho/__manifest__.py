dbd4f3ac01e652c1fb9af379a1027868a454c485
Hector Herrera <hector.herrera.mx@gmail.com>
2022-05-30 20:22:48 -0600

    [ADD]iho add po values in usd

--- iho/__manifest__.py
+++ iho/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018, Jarsa Sistemas, S.A. de C.V.
-# Copyright 2020,2021, MtNet Services, S.A. de C.V.
+# Copyright 2020,2021,2022 MtNet Services, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
